42e926b78cab74d30cafc7c01069ec26dcade3fc
Douglas Maia <douglasmai4@protonmail.com>
2024-01-18 15:23:05 -0300

    feat: Created products controller for respond all products or one

---
+++ backend/src/controllers/products.controller.js
@@ -0,0 +1,20 @@
+const { productsService } = require('../services');
+
+const findAll = async (_req, res) => {
+  const { status, data } = await productsService.findAll();
+
+  return res.status(status).json(data);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+
+  const { status, data } = await productsService.findById(id);
+
+  return res.status(status).json(data);
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
