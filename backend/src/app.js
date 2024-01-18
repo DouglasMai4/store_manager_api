44d4568e9282386d0a44a4e45e3805cda68797ec
Douglas Maia <douglasmai4@protonmail.com>
2024-01-18 15:23:51 -0300

    feat: Added products route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const { productsRoute } = require('./routes');
+
 const app = express();
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
 });
+
+// Products Route
+app.use('/products', productsRoute);
 
 module.exports = app;
